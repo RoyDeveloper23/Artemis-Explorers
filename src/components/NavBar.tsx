5b85960595c4a9d9ed3fa57817192a4233c6016e
RoyDeveloper23 <royerazo23@gmail.com>
2024-08-11 13:09:42 -0500

    bug arreglado

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+// import { useRef } from "react";
 
 // import { useEffect } from "react";
 // import { PictureApi } from "../services/PictureApi";
